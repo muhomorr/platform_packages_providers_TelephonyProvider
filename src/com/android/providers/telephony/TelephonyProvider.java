8407322481ef6d89ffe421cb51e4fea44a0af7b5
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-10-03 00:09:06 +0300

    reorder ExtSimState changes
    Change-Id: I3e4dfcbfdce158978aae890b87c184866623ac70

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -4225,6 +4225,7 @@ public class TelephonyProvider extends ContentProvider
                         Telephony.SimInfo.COLUMN_EXT_SIM_STATE,
                         backedUpSimInfoEntry.getString(Telephony.SimInfo.COLUMN_EXT_SIM_STATE,
                                 DEFAULT_STRING_COLUMN_VALUE));
+
             }
             if (backupDataFormatVersion >= 73 << 16) {
                 contentValues.put(
